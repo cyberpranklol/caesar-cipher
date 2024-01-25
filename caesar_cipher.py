2f57c77b49081088b38439642be2e6ec5872a323
cyberpranklol <kljajic.stefan96@gmail.com>
2024-01-25 18:11:35 +0100

    *

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,5 +1,4 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
   if cipher_direction == "decode":
@@ -21,7 +20,7 @@ while not should_end:
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
   text = input("Type your message:\n").lower()
   shift = int(input("Type the shift number:\n"))
-  shift = shift % 26
+  shift = shift % 26 
 
   caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
